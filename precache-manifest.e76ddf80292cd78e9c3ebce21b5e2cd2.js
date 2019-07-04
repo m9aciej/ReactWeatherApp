add1feb2af48ac503b99ad6300463ca6cd34a409
Maciej Szczurek <m9aciej@gmail.com>
2019-07-05 01:50:09 +0200

    Updates

---
+++ precache-manifest.e76ddf80292cd78e9c3ebce21b5e2cd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a11e9d73b4043f805417b78c97c6b7",
+    "url": "/ReactWeatherApp/index.html"
+  },
+  {
+    "revision": "3a82b59c4784b2d374b4",
+    "url": "/ReactWeatherApp/static/css/main.64bbf993.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ReactWeatherApp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3a82b59c4784b2d374b4",
+    "url": "/ReactWeatherApp/static/js/main.9f96069f.chunk.js"
+  },
+  {
+    "revision": "e76b96d066e6e484f785",
+    "url": "/ReactWeatherApp/static/js/runtime~main.cdf7d87e.js"
+  }
+]);
